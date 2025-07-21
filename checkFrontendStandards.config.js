5119c6eb3c58476ffde504266e56bf7ecf270c0e
Juan David Peña <juan.penaa@sofka.com.co>
2025-07-21 15:09:52 -0500

    chore: update version to 0.0.7 in README and config file

--- checkFrontendStandards.config.js
+++ checkFrontendStandards.config.js
@@ -22,7 +22,8 @@
  * - Simplest approach for adding a few custom rules
  *
  * @author Juan David Peña
- * @version 4.9.0
+ * @version 0.0.7
+ * @license MIT
  * @since 2024-01-15
  * @see {@link ./checkFrontendStandards.mjs} Main validation script
  * @see {@link ./checkFrontendStandards.types.js} Type definitions
